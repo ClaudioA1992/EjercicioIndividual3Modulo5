f9ffee11c400af18fd62311cc56498bb37123bcb
Claudio <claudioan00@gmail.com>
2023-07-05 20:18:20 -0400

    Ejercicio individual 3 Módulo 5

---
+++ app/src/main/java/cl/awakelab/ejercicioindividual3modulo5/MainActivity.java
@@ -0,0 +1,20 @@
+package cl.awakelab.ejercicioindividual3modulo5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cl.awakelab.ejercicioindividual3modulo5.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
